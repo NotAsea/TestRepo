4ad0046abd98af31abf2f50699ba6e026e4ed62c
NotAsea <luonghaic1166@gmail.com>
2024-01-08 16:31:24 +0700

    update migration

---
+++ src/TestRepo.Data/Migrations/20240108090343_Soft_Delete_Account.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TestRepo.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Soft_Delete_Account : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Accounts",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Accounts");
+        }
+    }
+}
